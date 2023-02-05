ad8691697a78eadc6ea80247f1a39ef9fe89898f
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2023-02-05 13:18:43 +0900

    chore: import fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        ibarra: ['Ibarra Real Nova, serif', ...defaultTheme.fontFamily.ibarra],
+        sans: ['Public Sans, sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
