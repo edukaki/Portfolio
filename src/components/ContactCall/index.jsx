13ae4d1971768207fa5b0fb132055114531eda90
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2023-02-07 19:54:01 +0900

    feat: create contactCall

---
+++ src/components/ContactCall/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Button from '../Button';
+
+const ContactCall = () => {
+  return (
+    <section className="py-16">
+      <h1 className="text-center pb-10">
+        Interested in doing a project together?
+      </h1>
+      <Button text="Contact me" secondary />
+    </section>
+  );
+};
+
+export default ContactCall;
