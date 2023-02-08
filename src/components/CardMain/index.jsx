fb60eea0b980fb8e273156658e2e99ad0048eb88
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2023-02-08 19:51:43 +0900

    feat(cardMain): add responsiveness for tablets

--- src/components/CardMain/index.jsx
+++ src/components/CardMain/index.jsx
@@ -4,16 +4,24 @@ import Button from '../Button';
 
 const CardMain = (props) => {
   return (
-    <section>
+    <section
+      className={`grid grid-flow-row ${
+        props.flow === 'normal'
+          ? 'md:grid-cols-2 md:gap-16'
+          : props.flow === 'reverse'
+          ? 'flex-row-reverse'
+          : ''
+      }`}
+    >
       <img src={props.img} alt={props.alt} className="w-full" />
       <div
         className={`flex flex-col gap-6 py-10 ${
           props.border === 1
             ? 'border-light-grey border-b-2'
             : props.border === 2
-            ? 'border-light-grey border-y-2 mt-8'
+            ? `border-light-grey border-y-2 ${props.flow ? '' : 'mt-8'}`
             : ''
-        }`}
+        } `}
       >
         {props.titleWeight === 'bold' ? (
           <h2>{props.title}</h2>
@@ -21,20 +29,21 @@ const CardMain = (props) => {
           <h3>{props.title}</h3>
         )}
         <p className="pt-5 leading-8">{props.text}</p>
-        {props.button && <Button text={props.button} secondary />}
+        {props.button && <Button text={props.button} secondary margin="ml-0" />}
       </div>
     </section>
   );
 };
 
 CardMain.propTypes = {
-  img: PropTypes.node,
   alt: PropTypes.string,
-  titleWeight: PropTypes.string,
-  title: PropTypes.string.isRequired,
-  text: PropTypes.string.isRequired,
   border: PropTypes.number,
   button: PropTypes.string,
+  flow: PropTypes.string,
+  img: PropTypes.node,
+  text: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  titleWeight: PropTypes.string,
 };
 
 export default CardMain;
