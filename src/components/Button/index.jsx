71e52de35afc75b22aa8b84ed08c7464be6365f9
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2023-02-08 18:35:52 +0900

    feat(button): add properties

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -5,16 +5,17 @@ import { ReactComponent as DownArrows } from '../../image/icons/down-arrows.svg'
 const Button = (props) => {
   return (
     <button
-      className={`group flex flex-row m-auto tracking-[2px] ${
+      type={props.submit && 'submit'}
+      className={`group flex flex-row tracking-[2px] ${
+        !props.margin ? 'm-auto' : props.margin
+      } ${
         props.secondary
           ? 'py-4 text-grayish-dark-blue bg-white border-opacity-80 border-grayish-dark-blue border-2 hover:text-white hover:bg-grayish-dark-blue disabled:border-very-light-grey'
           : 'text-white bg-dark-blue hover:bg-light-cyan disabled:bg-light-grey disabled:text-grayish-dark-blue'
       }`}
     >
       <div
-        className={`p-5 bg-black bg-opacity-10 ${
-          props.secondary ? 'hidden' : ''
-        }`}
+        className={`p-5 bg-black bg-opacity-10 ${props.secondary && 'hidden'}`}
       >
         <DownArrows className="stroke-light-cyan group-hover:stroke-white" />
       </div>
@@ -25,7 +26,9 @@ const Button = (props) => {
 
 Button.propTypes = {
   text: PropTypes.string.isRequired,
+  margin: PropTypes.string,
   secondary: PropTypes.bool,
+  submit: PropTypes.bool,
 };
 
 export default Button;
