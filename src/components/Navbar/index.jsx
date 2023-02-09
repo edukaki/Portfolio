dd49fdf874689ce33e76e54955fecc660fc90620
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2023-02-09 18:46:48 +0900

    feat(navbar): add responsiveness for tablets && desktops

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,21 +1,29 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import { ReactComponent as Logo } from '../../image/logo.svg';
 import BtnHamburger from './BtnHamburger';
 
 const Navbar = () => {
   const [hamburger, setHamburger] = useState(false);
   return (
-    <nav className="flex flex-row justify-between p-8 relative">
+    <nav className="flex flex-row justify-between md:items-center">
       <Logo className="fill-grayish-dark-blue" />
       <ul
         className={`${
-          !hamburger ? 'opacity-0' : 'opacity-100 '
+          !hamburger ? 'opacity-0' : 'opacity-100'
         } absolute top-[100%] right-0 py-6 px-14 bg-grayish-dark-blue text-white tracking-[2px] text-center uppercase transition-all duration-300
+        md:opacity-100 md:flex md:flex-row md:top-auto md:right-auto md:relative md:bg-transparent md:text-inherit md:px-0
         `}
       >
         <li className="p-4">
-          <Link to="/">Home</Link>
+          <NavLink
+            to="/"
+            className={(isActive) =>
+              isActive ? 'text-light-cyan font-bold' : ''
+            }
+          >
+            Home
+          </NavLink>
         </li>
         <li className="p-4">
           <Link to="/portfolio">Portfolio</Link>
