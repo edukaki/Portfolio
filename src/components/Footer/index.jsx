fe822b051a8b66cca4be7620e03604f9e7e97e81
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2023-02-09 17:43:01 +0900

    feat(footer): add responsiveness to mobile && desktop

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -6,38 +6,40 @@ import { ReactComponent as Github } from '../../image/icons/github.svg';
 import { ReactComponent as Twitter } from '../../image/icons/twitter.svg';
 const Footer = () => {
   return (
-    <footer className="flex flex-col bg-grayish-dark-blue">
-      <ul className="py-6 px-14 text-white tracking-[2px] text-center uppercase">
-        <li className="p-5">
-          <Logo className="fill-very-light-grey m-auto" />
-        </li>
-        <li className="p-4">
-          <Link to="/">Home</Link>
-        </li>
-        <li className="p-4">
-          <Link to="/portfolio">Portfolio</Link>
-        </li>
-        <li className="p-4">
-          <Link to="/contact">Contact me</Link>
-        </li>
-      </ul>
-      <ul className="flex flex-row justify-center pb-14">
-        <li className="p-4">
-          <Link to="https://github.com/edukaki">
-            <Github className="fill-very-light-grey" />
-          </Link>
-        </li>
-        <li className="p-4">
-          <Link to="/#">
-            <Twitter className="fill-very-light-grey" />
-          </Link>
-        </li>
-        <li className="p-4">
-          <Link to="https://www.linkedin.com/in/eduardo-arakaki/">
-            <Linkedin className="fill-very-light-grey" />
-          </Link>
-        </li>
-      </ul>
+    <footer>
+      <div className="flex flex-col md:flex-row md:justify-between md:items-center 2xl:max-w-[77%] m-auto">
+        <ul className="py-6 px-14 text-white tracking-[2px] text-center uppercase md:flex md:flex-row md:items-center md:px-0">
+          <li className="p-5">
+            <Logo className="fill-very-light-grey m-auto" />
+          </li>
+          <li className="p-4">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="p-4">
+            <Link to="/portfolio">Portfolio</Link>
+          </li>
+          <li className="p-4">
+            <Link to="/contact">Contact me</Link>
+          </li>
+        </ul>
+        <ul className="flex flex-row justify-center pb-14 md:items-center md:p-0">
+          <li className="p-4">
+            <Link to="https://github.com/edukaki">
+              <Github className="fill-very-light-grey" />
+            </Link>
+          </li>
+          <li className="p-4">
+            <Link to="/#">
+              <Twitter className="fill-very-light-grey" />
+            </Link>
+          </li>
+          <li className="p-4">
+            <Link to="https://www.linkedin.com/in/eduardo-arakaki/">
+              <Linkedin className="fill-very-light-grey" />
+            </Link>
+          </li>
+        </ul>
+      </div>
     </footer>
   );
 };
