295a24d0dec45851e2029c93e8372f7e5f1e27fb
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2023-02-12 02:33:05 +0900

    feat(router): add HeadingAbout

--- src/Routes/router.js
+++ src/Routes/router.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 import App from '../App';
+import HeadingAbout from '../components/HeadingAbout';
 import Layout from '../Layout/Layout';
 
 export const router = createBrowserRouter([
@@ -9,7 +10,12 @@ export const router = createBrowserRouter([
     children: [
       {
         path: '/',
-        element: <App />,
+        element: (
+          <>
+            <HeadingAbout text="Hey, I’m Eduardo Arakaki and I love building beautiful websites" />
+            <App />,
+          </>
+        ),
       },
     ],
   },
